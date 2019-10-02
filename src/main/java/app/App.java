5ba0e0f1f534cd282e29185e56d5266bfda0aaba
Williampsp1 <wlopez@unomaha.edu>
2019-10-02 17:14:17 -0500

    Done by willl

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,9 +1,4 @@
 package app;
-import javax.xml.ws.soap.Addressing;
-
-import org.eclipse.jetty.util.PathWatcher.Config;
-
-import io.javalin.plugin.rendering.vue.VueComponent;
 
 import io.javalin.Javalin;
 
@@ -23,7 +18,7 @@ public final class App {
     public static void main(String[] args) {
 
         
-        Javalin app = Javalin.create(config -> {
+        Javalin.create(config -> {
             config.addSinglePageRoot("/", "/vue/index.html");
         }).start(7000);            
 
