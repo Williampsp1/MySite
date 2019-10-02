bffdc840841b1f4b6820b3cfe445706a577d2a73
Williampsp1 <wlopez@unomaha.edu>
2019-10-02 13:37:29 -0500

    commit message from terminal

---
+++ src/main/java/app/App.java
@@ -0,0 +1,19 @@
+package app;
+import io.javalin.Javalin;
+
+
+/**
+ * Hello world!
+ */
+public
+ class App {
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
