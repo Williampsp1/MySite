eeca867aeda40bb1aa403da13fa4436d48704d13
Williampsp1 <43650249+Williampsp1@users.noreply.github.com>
2019-10-01 21:41:20 -0500

    Add files via upload

---
+++ MySite/src/main/java/app/App.java
@@ -0,0 +1,20 @@
+package app;
+import io.javalin.Javalin;
+
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
