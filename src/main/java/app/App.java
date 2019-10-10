a0abb206fd8bf313867ce67f55b99906af565e03
Williampsp1 <wlopez@unomaha.edu>
2019-10-10 18:04:26 -0500

    Added login

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -18,12 +18,21 @@ public final class App {
     public static void main(String[] args) {
 
         
-        Javalin.create(config -> {
+       Javalin app = Javalin.create(config -> {
           //  config.addSinglePageRoot("/", "/vue/Start.html");
             config.addStaticFiles("/vue");
-        }).start(7000);            
+        }).start(7000); 
 
+        app.get("submit", ctx -> {
+           // console.log(`tried to login with url params ${req.query.username} and ${req.query.password}`);
+          
+            if (ctx.req.username == "will" && req.query.password == "pass") {
+              ctx.res.sendFile(path.join(__dirname, "Part1.html"));
+            }
+            else{
+              ctx.res.sendFile(ctx.path.join(__dirname, "unauthorized.html"));
+            }
+          })
         
-                
-    }
+}
 }
