f48f76f7f789f2b007bcf22a5d18f54a54c8c7da
Williampsp1 <wlopez@unomaha.edu>
2019-10-24 17:22:50 -0500

    cleaned up some code

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -2,12 +2,15 @@ package app;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.eclipse.jetty.util.log.Log;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -18,7 +21,7 @@ import io.javalin.Javalin;
 /**
  * Hello world!
  */
-public final class App extends HttpServlet{
+public final class App extends HttpServlet {
 
   private App() {
   }
@@ -29,8 +32,9 @@ public final class App extends HttpServlet{
    * @param args The arguments of the program.
    */
   static String title;
+  static String price;
 
-  public static void main(String[] args)  throws IOException {
+  public static void main(String[] args) throws IOException {
 
     Javalin app = Javalin.create(config -> {
       // config.addSinglePageRoot("/", "/vue/Start.html");
@@ -39,69 +43,41 @@ public final class App extends HttpServlet{
 
     }).start(7000);
 
-    app.post("/login", ctx -> {
-      // console.log(`tried to login with url params ${req.query.username} and
-      // ${req.query.password}`);
-
-      if ((ctx.formParam("uname").equals("Will")) && (ctx.formParam("psw").equals("pass"))) {
-        ctx.redirect("Part1.html");
-      } else {
-
-        ctx.redirect("indexPrac.html");
-
-      }
-    });
-
+    app.
     String url = "https://www.amazon.com/Intel-i5-9600K-Desktop-Processor-Unlocked/dp/B07HHLX1R8/ref=sr_1_4?keywords=cpu+i5&qid=1571698422&s=electronics&sr=1-4";
-
-    
     Document document = Jsoup.connect(url).get();
-   Elements question = document.select("#priceblock_ourprice");
-    String price = ("Price is " + question.html());
-
-System.out.println(new File(".").getAbsoluteFile());
+    Elements question = document.select("#priceblock_ourprice");
+     price = ("Price is " + question.html());
 
     File input = new File("src/main/resources/HTML files/Prices.html");
-   /*  Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
-    
-    org.jsoup.nodes.Element div = doc.select("div").first(); // <div></div>
-    div.html("<p>lorem ipsum</p>"); // <div><p>lorem ipsum</p></div>
-div.prepend("<p>First</p>");
-div.append("<p>Last</p>");
-// now: <div><p>First</p><p>lorem ipsum</p><p>Last</p></div>
+   
 
-org.jsoup.nodes.Element span = doc.select("span").first(); // <span>One</span>
-span.wrap("<li><a href='http://example.com/'></a></li>");*/
-// now: <li><a href="http://example.com"><span>One</span></a></li>
-   // title = document.title();
-  //  System.out.println(spanElement.text());
- 
-  Document doc = Jsoup.parse(input,"utf-8");
-Element p = doc.select("div").first();
-p.text("My Example Text");
+    Document doc = Jsoup.parse(input, "utf-8");
+    Element p = doc.select("div").first();
+    p.text("My Example Text");
 
-    app.ws("/websocket/:path",ws -> {
+    app.ws("/websocket/:path", ws -> {
       ws.onConnect(ctx -> {
         ctx.send(price);
       });
     });
-   // window.location ="PC needs.html" + title;
-     
-  
+   
 
-
-		
-    }
-  
-
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String message = "Hello World";
-        request.setAttribute("message", message); // This will be available as ${message}
-        request.getRequestDispatcher("/PC needs.html").forward(request, response);
-    
-
-    }
   }
+
+  
+    @Override protected void doGet(HttpServletRequest request,
+              HttpServletResponse response) throws ServletException, IOException
+              { String message = "Hello World"; request.setAttribute("message",
+              message); // This will be available as ${message}
+              request.getRequestDispatcher("/PC needs.html").forward(request,
+              response);
+              
+              }
+   
+
+}
+
+ 
+  
 
