c39c01c25dcc57992cc513a939093da2e62b1c6e
William <Williampsp1@gmail.com>
2019-10-27 23:43:24 -0500

    Implemented Amazon prices

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -2,15 +2,7 @@ package app;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.eclipse.jetty.util.log.Log;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -21,7 +13,7 @@ import io.javalin.Javalin;
 /**
  * Hello world!
  */
-public final class App extends HttpServlet {
+public final class App {
 
   private App() {
   }
@@ -37,44 +29,32 @@ public final class App extends HttpServlet {
   public static void main(String[] args) throws IOException {
 
     Javalin app = Javalin.create(config -> {
-      // config.addSinglePageRoot("/", "/vue/Start.html");
-      config.addStaticFiles("/HTML files");
-      config.addStaticFiles("/Images");
+      config.addStaticFiles("./HTML files");
+      config.addStaticFiles("./Images");
 
     }).start(7000);
 
-    app.
+    
     String url = "https://www.amazon.com/Intel-i5-9600K-Desktop-Processor-Unlocked/dp/B07HHLX1R8/ref=sr_1_4?keywords=cpu+i5&qid=1571698422&s=electronics&sr=1-4";
     Document document = Jsoup.connect(url).get();
     Elements question = document.select("#priceblock_ourprice");
-     price = ("Price is " + question.html());
+    price = ("Price is " + question.html());
+    System.out.print(price);
 
-    File input = new File("src/main/resources/HTML files/Prices.html");
-   
+    app.get("/ajax", ctx -> {
 
-    Document doc = Jsoup.parse(input, "utf-8");
-    Element p = doc.select("div").first();
-    p.text("My Example Text");
+            ctx.result(price);
+
+    });
+
 
     app.ws("/websocket/:path", ws -> {
       ws.onConnect(ctx -> {
         ctx.send(price);
       });
     });
-   
 
   }
-
-  
-    @Override protected void doGet(HttpServletRequest request,
-              HttpServletResponse response) throws ServletException, IOException
-              { String message = "Hello World"; request.setAttribute("message",
-              message); // This will be available as ${message}
-              request.getRequestDispatcher("/PC needs.html").forward(request,
-              response);
-              
-              }
-   
 
 }
 
