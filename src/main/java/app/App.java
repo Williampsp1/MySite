df720e3b0abdc6a0d0dbee4a4749076c6c9e54a8
Williampsp1 <wlopez@unomaha.edu>
2019-11-01 17:00:15 -0500

    Almost finished with PC needs

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -42,32 +42,19 @@ public final class App {
     price = ("Price is " + question.html());
     System.out.print(price);
 
-/*
-    org.jsoup.Connection.Response response = Jsoup.connect(
-        "https://www.amazon.com/Intel-i5-9600K-Desktop-Processor-Unlocked/dp/B07HHLX1R8/ref=sr_1_4?keywords=cpu+i5&qid=1571698422&s=electronics&sr=1-4")
-    .userAgent("Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2")
-    .timeout(0).ignoreHttpErrors(true)
-    .execute();
-
-    String u = response.body();
-    Document p = Jsoup.parse(u);
-    Element pr = p.body();
-    Elements pricee = p.select("#priceblock_ourprice");
-     price = ("Price is " + pricee.html());
-*/
     app.get("/ajax", ctx -> {
 
             ctx.result(price);
 
     });
 
-
+/*
     app.ws("/websocket/:path", ws -> {
       ws.onConnect(ctx -> {
         ctx.send(price);
       });
     });
-
+    */
   }
 
 }
